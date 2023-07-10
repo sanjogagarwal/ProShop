f664549e3b4568bccc6fe5e356ee487e7781320e
Sanjog Agarwal <sanjogagarwal@Sanjogs-MacBook-Pro.local>
2023-07-11 01:39:14 +0530

    connecting with database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+
 const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to MongoDB database
 
 const app = express();
 
@@ -10,13 +14,6 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => {console.log(`Server is running on port ${port}`)});
