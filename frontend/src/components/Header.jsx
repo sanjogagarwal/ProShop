4909cf48508b547b459de2e00784e0ab88c7fabb
Sanjog Agarwal <sanjogagarwal@Sanjogs-MacBook-Pro.local>
2023-07-10 12:29:55 +0530

    getting data from backend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Navbar, Nav, Container, NavLink } from 'react-bootstrap';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+import logo from '../assets/logo.png';
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+            <LinkContainer to='/'>
+                <Navbar.Brand>
+                    <img src={logo} alt='ProShop'/>
+                    ProShop
+                </Navbar.Brand>
+            </LinkContainer>  
+            <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+            <NavbarCollapse id='basic-navbar-nav'>
+                <Nav className='ms-auto'>
+                    <LinkContainer to='/cart'>
+                        <NavLink>
+                            <FaShoppingCart/> Cart
+                        </NavLink>
+                    </LinkContainer>
+                    <LinkContainer to='/login'>
+                        <NavLink>
+                            <FaUser/> Sign In
+                        </NavLink>
+                    </LinkContainer>
+                </Nav>
+            </NavbarCollapse>
+        </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
