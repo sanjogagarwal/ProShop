4145c23bfa42b786c9677c301c29644b17f600fc
Sanjog Agarwal <sanjogagarwal@Sanjogs-MacBook-Pro.local>
2023-07-13 22:37:34 +0530

    redux integration and cart function

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Navbar, Nav, Container, NavLink } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container, NavLink } from 'react-bootstrap';
 import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png';
 
 const Header = () => {
+
+    const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -22,6 +26,13 @@ const Header = () => {
                     <LinkContainer to='/cart'>
                         <NavLink>
                             <FaShoppingCart/> Cart
+                            {
+                                cartItems.length > 0 && (
+                                    <Badge pill bg='success' style={{marginLeft:'5px'}}>
+                                        { cartItems.reduce((a, c) => a+c.qty, 0)}
+                                    </Badge>
+                                )
+                            }
                         </NavLink>
                     </LinkContainer>
                     <LinkContainer to='/login'>
