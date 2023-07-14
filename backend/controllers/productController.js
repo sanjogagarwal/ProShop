a57759160e38b11bb7a6b5e4c3e6e0ad1b5d8e2c
Sanjog Agarwal <sanjogagarwal@Sanjogs-MacBook-Pro.local>
2023-07-15 00:38:47 +0530

    backend authentication

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,9 +15,10 @@ const getProducts = asyncHandler(async (req, res) => {
 const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
     if(product) {
-        res.json(product);
+        return res.json(product);
     }
-    res.status(404).json({message: 'Product not found' });
+    res.status(404);
+    throw new Error('Resource not found');
 });
 
 export { getProducts, getProductById };
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorHandler.js';
 
 const port = process.env.PORT || 5000;
 
@@ -10,10 +13,22 @@ connectDB(); // Connect to MongoDB database
 
 const app = express();
 
+//  Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//  Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
+//  Error middleware
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {console.log(`Server is running on port ${port}`)});
